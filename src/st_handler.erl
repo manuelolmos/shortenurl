1980e308a3344224307b6a6c1c247cfc24649906
Manuel Olmos Gil <manuolmos88@gmail.com>
2018-01-04 17:09:27 -0300

    Fix issue on handler

--- src/st_handler.erl
+++ src/st_handler.erl
@@ -3,14 +3,14 @@
 -export([init/2]).
 
 init(Req0 = #{method := <<"POST">>}, State) ->
-	{ok, LongUrl, Req} = cowboy_req:read_body(Req0),
+	{ok, LongUrl, Req1} = cowboy_req:read_body(Req0),
 	{ok, Random} = st_db:save(LongUrl),
 	%% Replace url with the correct one
 	ShortUrl = erlang:iolist_to_binary([<<"http://localhost:8080/">>, Random]),
-	Req = cowboy_req:reply(200,
+	Req2 = cowboy_req:reply(200,
 		#{<<"content-type">> => <<"text/plain">>}, 
-		ShortUrl, Req0),
-    {ok, Req, State};
+		ShortUrl, Req1),
+    {ok, Req2, State};
 init(Req0, State) ->
     Req = cowboy_req:reply(200, #{
         <<"content-type">> => <<"text/plain">>

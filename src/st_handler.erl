fc7efca2cc1020b94043b661b423867f748e38b3
Manuel Olmos Gil <manuolmos88@gmail.com>
2018-01-05 13:10:28 -0300

    Update st_handler.erl

--- src/st_handler.erl
+++ src/st_handler.erl
@@ -5,7 +5,6 @@
 init(Req0 = #{method := <<"POST">>}, State) ->
 	{ok, LongUrl, Req1} = cowboy_req:read_body(Req0),
 	{ok, Random} = st_db:save(LongUrl),
-	%% Replace url with the correct one
 	ShortUrl = erlang:iolist_to_binary([<<"http://localhost:8080/">>, Random]),
 	Req2 = cowboy_req:reply(200,
 		#{<<"content-type">> => <<"text/plain">>}, 

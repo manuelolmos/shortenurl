b5b2c9666545991fcee81fb143990da641cd7701
Manuel Olmos Gil <manuolmos88@gmail.com>
2018-04-18 23:14:08 -0300

    Update code style

--- src/st_db.erl
+++ src/st_db.erl
@@ -1,47 +1,45 @@
 -module(st_db).
 
--export([
-		create/0,
-		save/1,
-		get/1
-		]).
+-export([create/0,
+         save/1,
+         get/1]).
 
 create() ->
-	st_short_long_url = ets:new(st_short_long_url, [set, public, named_table]),
-	st_long_short_url = ets:new(st_long_short_url, [set, public, named_table]),
-	ok.
+    st_short_long_url = ets:new(st_short_long_url, [set, public, named_table]),
+    st_long_short_url = ets:new(st_long_short_url, [set, public, named_table]),
+    ok.
 
 save(LongUrl) ->
-	case get(st_long_short_url, LongUrl) of
-		{ok, Random} ->
-			{ok, Random};
-		{error, not_found} ->
-			Random = generate_random(),
-			case ets:insert(st_short_long_url, {Random, LongUrl}) of
-				true ->
-					case ets:insert(st_long_short_url, {LongUrl, Random}) of
-						true ->
-							{ok, Random};
-						_ ->
-							ets:delete(st_short_long_url, Random),
-							{error, ets_insertion_error}
-					end;
-				_ ->
-					{error, ets_insertion_error}
-			end
-	end.
-	
+    case get(st_long_short_url, LongUrl) of
+        {ok, Random} ->
+            {ok, Random};
+        {error, not_found} ->
+            Random = generate_random(),
+            case ets:insert(st_short_long_url, {Random, LongUrl}) of
+                true ->
+                    case ets:insert(st_long_short_url, {LongUrl, Random}) of
+                        true ->
+                            {ok, Random};
+                        _ ->
+                            ets:delete(st_short_long_url, Random),
+                            {error, ets_insertion_error}
+                    end;
+                _ ->
+                    {error, ets_insertion_error}
+            end
+    end.
+
 
 get(Random) ->
-	get(st_short_long_url, Random).
+    get(st_short_long_url, Random).
 
 get(TableName, Key) ->
-	case ets:lookup(TableName, Key) of
-		[{Key, Value}] ->
-			{ok, Value};
-		_ ->
-			{error, not_found}
-	end.
+    case ets:lookup(TableName, Key) of
+        [{Key, Value}] ->
+            {ok, Value};
+        _ ->
+            {error, not_found}
+    end.
 
 generate_random() ->
-	base64:encode(crypto:strong_rand_bytes(10)).
+    base64:encode(crypto:strong_rand_bytes(10)).
--- src/st_handler.erl
+++ src/st_handler.erl
@@ -3,29 +3,29 @@
 -export([init/2]).
 
 init(Req0 = #{method := <<"POST">>}, State) ->
-	{ok, LongUrl, Req1} = cowboy_req:read_body(Req0),
-	{ok, Random} = st_db:save(LongUrl),
-	Path = iolist_to_binary(cowboy_req:uri(Req1)),
-	ShortUrl = erlang:iolist_to_binary([Path, Random]),
-	Req2 = cowboy_req:reply(200,
-		#{<<"content-type">> => <<"text/plain">>}, 
-		ShortUrl, Req1),
+    {ok, LongUrl, Req1} = cowboy_req:read_body(Req0),
+    {ok, Random} = st_db:save(LongUrl),
+    Path = iolist_to_binary(cowboy_req:uri(Req1)),
+    ShortUrl = erlang:iolist_to_binary([Path, Random]),
+    Req2 = cowboy_req:reply(200,
+                            #{<<"content-type">> => <<"text/plain">>}, 
+                            ShortUrl, Req1),
     {ok, Req2, State};
 init(Req0 = #{method := <<"GET">>}, State) ->
-	case cowboy_req:binding(shortenurl, Req0) of
-		undefined ->
-			lager:error("There is no shorturl sent"),
-			{ok, cowboy_req:reply(404, Req0), State};
-		ShortUrl ->
-			case st_db:get(ShortUrl) of
-				{ok, LongUrl} ->
-					Req1 = cowboy_req:reply(301, 
-						#{<<"location">> => LongUrl}, Req0),
-					{ok, Req1, State};
-				{error, not_found} ->
-					lager:warning("Shorturl not found"),
-					{ok, cowboy_req:reply(404, Req0), State}
-			end
-	end;
+    case cowboy_req:binding(shortenurl, Req0) of
+        undefined ->
+            lager:error("There is no shorturl sent"),
+            {ok, cowboy_req:reply(404, Req0), State};
+        ShortUrl ->
+            case st_db:get(ShortUrl) of
+                {ok, LongUrl} ->
+                    Req1 = cowboy_req:reply(301, 
+                                            #{<<"location">> => LongUrl}, Req0),
+                    {ok, Req1, State};
+                {error, not_found} ->
+                    lager:warning("Shorturl not found"),
+                    {ok, cowboy_req:reply(404, Req0), State}
+            end
+    end;
 init(Req0, State) ->
     {ok, cowboy_req:reply(400, Req0), State}.
